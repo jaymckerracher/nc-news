2c729943ea08aa876aa040dbf41ff45c430fa64c
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2023-11-23 12:01:44 +0000

    add functionality to endpoint and add endpoint to endpoints.json

--- models/comments-model.js
+++ models/comments-model.js
@@ -30,5 +30,20 @@ exports.checkValidComment = (username, body) => {
 }
 
 exports.deleteComment = (id) => {
-    console.log('we are getting here!')
+    return db
+        .query(`
+        DELETE FROM comments
+        WHERE comment_id = $1
+        RETURNING *;
+        `, [id])
+        .then(({rows}) => {
+            const deletedComment = rows[0];
+            if (!deletedComment) {
+                return Promise.reject({
+                    status: 404,
+                    msg: 'Comment Not Found'
+                })
+            }
+            return deletedComment;
+        })
 }
