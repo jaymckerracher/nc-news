920e5dca8e49e2c7a64eb96235de58c0f4e73460
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2023-11-24 14:42:40 +0000

    adds comment_count property to the /api/articles/:article_id path

--- models/articles-model.js
+++ models/articles-model.js
@@ -2,7 +2,13 @@ const db = require(`${__dirname}/../db/connection`);
 
 exports.selectArticle = (id) => {
     return db
-        .query(`SELECT * FROM articles WHERE article_id = $1;`, [id])
+        .query(`
+        SELECT articles.*, COUNT(comments.comment_id) AS comment_count
+        FROM articles
+        LEFT JOIN comments
+        ON articles.article_id = comments.article_id
+        WHERE articles.article_id = $1
+        GROUP BY articles.article_id;`, [id])
         .then(({rows}) => {
             const article = rows[0];
             if (!article) {
