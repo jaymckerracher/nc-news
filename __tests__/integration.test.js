3765995f877114bd9c528a3354cd9a1805ee9e52
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2023-11-24 14:57:56 +0000

    adding new test for comment_count field

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -91,9 +91,17 @@ describe('/api/articles/:article_id', () => {
                 body: "I find this existence challenging",
                 created_at: "2020-07-09T20:11:00.000Z",
                 votes: 100,
-                article_img_url:"https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-                comment_count: "11"
+                article_img_url:"https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
             })
+        })
+    });
+    test('GET: 200 responds with an object that contains a comment_count property, that contains the number of comments associated with the selected article', () => {
+        return request(app)
+        .get('/api/articles/1')
+        .expect(200)
+        .then(({body}) => {
+            const {article} = body
+            expect(article.comment_count).toBe('11');
         })
     });
     test('GET: 400 sends an appropriate status and error message when sent an invalid ID', () => {
